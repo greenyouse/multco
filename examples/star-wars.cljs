90e616b70bb94ba5fed318138e77f04d55f97b9e
greenyouse <greenyouse@hotmail.com>
2015-02-22 20:38:59 -0600

    Added DB Atom type

--- examples/star-wars.cljs
+++ examples/star-wars.cljs
@@ -7,17 +7,6 @@
 
 ;; NOTE: IndexedDB is disabled in private browsing mode
 
-(enable-console-print!)
-
-;; Define a clodexeddb database for your app. Use this schema whenever
-;; you use pldb-cache.
-(def test-db
-  (pc/setup "test"))
-
-;; Store each pldb database in separate atoms, we're only using one here.
-(def test-atom
-  (atom []))
-
 (pm/db-rel parent Parent Child)
 (pm/db-rel female Person)
 (pm/db-rel male Person)
@@ -26,67 +15,68 @@
 ;; or retractions will be saved and override this set of facts.
 ;; This allows a program to store its info persistently!
 ;; For reference: http://www.chartgeek.com/star-wars-family-tree/
-(def sw-characters-template
-  (pc/init-facts test-db "characters" test-atom
-                  [parent "Shmi Skywalker" "Anakin Skywalker"]
-                  [parent "Ruwee Naberrie" "Padme Amidala"]
-                  [parent "Jorbal Naberrie" "Padme Amidala"]
-                  [parent "Cliegg Lars" "Owen Lars"]
-                  [parent "Owen Lars" "Luke Skywalker"]
-                  [parent "Beru Lars" "Luke Skywalker"]
-                  [parent "Luke Skywalker" "Ben Skywalker"]
-                  [parent "Mara Jade" "Ben Skywalker"]
-                  [parent "Anakin Skywalker" "Luke Skywalker"]
-                  [parent "Padme Amidala" "Luke Skywalker"]
-                  [parent "Anakin Skywalker" "Princess Leia"]
-                  [parent "Padme Amidala" "Princess Leia"]
-                  [parent "Bail Organa" "Princess Leia"]
-                  [parent "Breha Organa" "Princess Leia"]
-                  [parent "Princess Leia" "Jaina Solo"]
-                  [parent "Princess Leia" "Jacen Solo"]
-                  [parent "Princess Leia" "Anakin Solo"]
-                  [parent "Han Solo" "Jaina Solo"]
-                  [parent "Han Solo" "Jacen Solo"]
-                  [parent "Han Solo" "Anakin Solo"]
+(def test-atom
+  (pc/db-atom "test" "woot" :facts
+              [[parent "Shmi Skywalker" "Anakin Skywalker"]
+               [parent "Ruwee Naberrie" "Padme Amidala"]
+               [parent "Jorbal Naberrie" "Padme Amidala"]
+               [parent "Cliegg Lars" "Owen Lars"]
+               [parent "Owen Lars" "Luke Skywalker"]
+               [parent "Beru Lars" "Luke Skywalker"]
+               [parent "Luke Skywalker" "Ben Skywalker"]
+               [parent "Mara Jade" "Ben Skywalker"]
+               [parent "Anakin Skywalker" "Luke Skywalker"]
+               [parent "Padme Amidala" "Luke Skywalker"]
+               [parent "Anakin Skywalker" "Princess Leia"]
+               [parent "Padme Amidala" "Princess Leia"]
+               [parent "Bail Organa" "Princess Leia"]
+               [parent "Breha Organa" "Princess Leia"]
+               [parent "Princess Leia" "Jaina Solo"]
+               [parent "Princess Leia" "Jacen Solo"]
+               [parent "Princess Leia" "Anakin Solo"]
+               [parent "Han Solo" "Jaina Solo"]
+               [parent "Han Solo" "Jacen Solo"]
+               [parent "Han Solo" "Anakin Solo"]
 
-                  [female "Shmi Skywalker"]
-                  [female "Jorbal Naberrie"]
-                  [female "Beru Lars"]
-                  [female "Mara Jade"]
-                  [female "Padme Amidala"]
-                  [female "Breha Organa"]
-                  [female "Princess Leia"]
-                  [female "Jaina Solo"]
+               [female "Shmi Skywalker"]
+               [female "Jorbal Naberrie"]
+               [female "Beru Lars"]
+               [female "Mara Jade"]
+               [female "Padme Amidala"]
+               [female "Breha Organa"]
+               [female "Princess Leia"]
+               [female "Jaina Solo"]
 
-                  [male "Cliegg Lars"]
-                  [male "Owen Lars"]
-                  [male "Ruwee Naberrie"]
-                  [male "Anakin Skywalker"]
-                  [male "Bail Organa"]
-                  [male "Ben Skywalker"]
-                  [male "Han Solo"]
-                  [male "Jacen Solo"]
-                  [male "Anakin Solo"]))
+               [male "Cliegg Lars"]
+               [male "Owen Lars"]
+               [male "Ruwee Naberrie"]
+               [male "Anakin Skywalker"]
+               [male "Bail Organa"]
+               [male "Ben Skywalker"]
+               [male "Han Solo"]
+               [male "Jacen Solo"]
+               [male "Anakin Solo"]]))
+
 
 ;; Find Luke's father (he has two because of adoption)
 (println (pm/with-db @test-atom
-            (lm/run* [q]
-              (parent q "Luke Skywalker")
-              (male q))))
+           (lm/run* [q]
+             (parent q "Luke Skywalker")
+             (male q))))
 
 ;; Find Luke's mom (step-mom too)
 (println (pm/with-db @test-atom
-            (lm/run* [q]
-              (parent q "Luke Skywalker")
-              (female q))))
+           (lm/run* [q]
+             (parent q "Luke Skywalker")
+             (female q))))
 
 ;; Luke's grandmothers
 (println (pm/with-db @test-atom
-            (lm/run* [q]
-              (lm/fresh [p]
-                (parent p "Luke Skywalker")
-                (parent q p)
-                (female q)))))
+           (lm/run* [q]
+             (lm/fresh [p]
+               (parent p "Luke Skywalker")
+               (parent q p)
+               (female q)))))
 
 ;; let's find all the father-child relations
 (println (pm/with-db @test-atom
@@ -99,11 +89,11 @@
 
 ;; Now let's add more facts to our database and see how the changes are
 ;; reflected in the queries
-(pc/add-facts! test-db "characters" test-atom
-                [parent "???" "Chewbacca"]
-                [parent "???" "Anakin Skywalker"]
-                [male "???"]
-                [male "Chewbacca"])
+(pc/add-facts! test-atom
+               [parent "???" "Chewbacca"]
+               [parent "???" "Anakin Skywalker"]
+               [male "???"]
+               [male "Chewbacca"])
 
 ;; So now we have people with unknown fathers in our data
 (println (pm/with-db @test-atom
@@ -112,11 +102,11 @@
 
 
 ;; Let's get rid of that and go back to the default
-(pc/remove-facts! test-db "characters" test-atom
-                [parent "???" "Chewbacca"]
-                [parent "???" "Anakin Skywalker"]
-                [male "???"]
-                [male "Chewbacca"])
+(pc/rm-facts! test-atom
+                  [parent "???" "Chewbacca"]
+                  [parent "???" "Anakin Skywalker"]
+                  [male "???"]
+                  [male "Chewbacca"])
 
 ;; Double-check that the facts are gone
 (println (pm/with-db @test-atom
@@ -126,16 +116,15 @@
 
 ;; What if we only care about one relationship and want to delete
 ;; everything else?
-(pc/reset-facts! test-db "characters" test-atom
-               [parent "???" "Jar Jar Binks"]
-               [female "???"]
-               [male "???"]
-               [male "Jar Jar Binks"])
+(pc/reset-facts! test-atom
+                 [parent "???" "Jar Jar Binks"]
+                 [female "???"]
+                 [male "???"]
+                 [male "Jar Jar Binks"])
 
 
-;; Try reloading the page and evaluting the default the template
-;; and everything above it. Now check to see if the change we made
-;; persisted.
+;; Try reloading the page and evaluting test-atom and the relations above
+;; it. Now check to see if the change we made persisted.
 (println (pm/with-db @test-atom
            (lm/run* [q]
              (male "Jar Jar Binks"))))
@@ -149,7 +138,7 @@
 
 ;; Yup, Jar Jar is the only thing in there (oh no) and everything worked!
 ;; If you want to delete this experiment do:
-(pc/clear test-db "characters")
+(pc/clear! test-atom)
 
 ;; and here's how to delete the entire database:
-(pc/rm-db "test")
+(pc/clear! test-atom :delete true)
--- src/pldb_cache/core.cljs
+++ src/pldb_cache/core.cljs
@@ -20,82 +20,136 @@
              (.log js/console "pldb-cache Error: improper message to transactor")))
       (recur))))
 
-;; taken from clodexeddb to save from having to include it in deps
-(defn setup
-  "Defines a new clientside database."
-  [name]
-  (transactor)
-  (cldb/setup name))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; DB Atom
+
+(defprotocol IDatabase
+  (-pldb-load [this facts]
+    "Defines a new clientside database.")
+  (-pldb-add! [this facts])
+  (-pldb-remove! [this facts])
+  (-pldb-reset! [this facts])
+  (-pldb-clear! [this delete]))
+
+;; state is the pldb database state, db-state is the value of cldb/setup
+;; FIXME: make names for state/db-state better to avoid confusion
+(deftype DBAtom [^:mutable state meta validator watches
+                 ^string db ^string store ^:mutable db-state]
+
+  IDatabase
+  (-pldb-load [this facts]
+    (transactor)
+    (set! db-state (cldb/setup db))
+    (cldb/get-query db-state "database" store
+                    (fn [e]
+                      (if (= e js/undefined)
+                        (-pldb-reset! this facts)
+                        (let [new-db (-> (get e "value")
+                                         (reader/read-string))]
+                          (-reset! this new-db))))))
+  (-pldb-add! [this facts]
+    (let [new-db (apply pldb/db-facts state facts)]
+      (put! trans-chan [db-state store new-db])
+      (-reset! this new-db)))
+  (-pldb-remove! [this facts]
+    (let [new-db (apply pldb/db-retractions state facts)]
+      (put! trans-chan [db-state store new-db])
+      (-reset! this new-db)))
+  (-pldb-reset! [this facts]
+    (let [new-db (apply pldb/db facts)]
+      (put! trans-chan [db-state store new-db])
+      (-reset! this new-db)))
+  (-pldb-clear! [this delete]
+    (cldb/rm-db db)
+    (set! state nil) ;clear the atom
+    (set! db-state nil)
+    (if-not delete
+      (-pldb-load this nil))) ;reload the database (just clears stores)
+
+  IReset
+  (-reset! [this new-value]
+    (when-not (nil? validator)
+      (assert (validator new-value) "Validator rejected reference state"))
+    (let [old-value state]
+      (set! state new-value)
+      (when-not (nil? watches)
+        (-notify-watches this old-value new-value))
+      new-value))
+
+  IAtom
+
+  IEquiv
+  (-equiv [o other] (identical? o other))
+
+  IDeref
+  (-deref [_] state)
+
+  IMeta
+  (-meta [_] meta)
+
+  IPrintWithWriter
+  (-pr-writer [_ writer opts]
+    (-write writer "#<DB-Atom: ")
+    (pr-writer state writer opts)
+    (-write writer ">"))
+
+  IWatchable
+  (-notify-watches [this oldval newval]
+    (doseq [[key f] watches]
+      (f key this oldval newval)))
+  (-add-watch [this key f]
+    (set! (.-watches this) (assoc watches key f))
+    this)
+  (-remove-watch [this key]
+    (set! (.-watches this) (dissoc watches key)))
+
+  IHash
+  (-hash [this] (goog/getUid this)))
+
+(defn db-atom
+  "This is a variation of the standard Clojure atom that holds a pldb database
+  in memory and caches any pldb updates to clientside storage. The first
+  argument, db, is the name of a clientside database for your project. The second
+  argument, store, is the name of a pldb store. Generally, a project should only
+  use one db to hold all of its pldb stores.
+
+  A new :facts keyword is used to define a template pldb database wrapped in a
+  sequential data structure (like a vector or list):
+
+  (db-atom \"db\" \"store\"
+           :facts [[some db items] [some more items]]
+           :meta metadata-map
+           :validator validate-fn)
+
+  These facts act as a default template for the pldb store. When a store is first
+  created, it will be populated with this set of facts. On subsequent reloads,
+  the pldb store will ignore these facts and use the previous database state
+  instead."
+  ;; HACK: facts shouldn't have to be a key, hard to get around that though
+  ([db store & {:keys [meta validator facts]}]
+   (let [a (DBAtom. nil meta validator nil db store nil)]
+     (-pldb-load a facts)
+     a)))
 
 (defn add-facts!
-  "Adds new facts and caches the new state.
+  "Adds new facts and caches the new state."
+  [a & facts]
+  (-pldb-add! a facts))
 
-  db            -- the IndexedDB database defined with clodexeddb.core.setup
-  name          -- the text name of ory database, used as a primary
-                    key for IndexedDB
-  logic-db-atom -- the pldb database being used, stored in an atom
-  facts         -- pldb facts to add"
-  [db name logic-db-atom & facts]
-  (let [new-db (apply pldb/db-facts @logic-db-atom facts)]
-    (put! trans-chan [db name new-db])
-    (reset! logic-db-atom new-db)))
-
-(defn remove-facts!
-  "Removes facts and stores the new state.
-
-  db            -- the IndexedDB database defined with clodexeddb.core.setup
-  name          -- the text name of your in-memory database, used as a primary
-                    key for IndexedDB
-  logic-db-atom -- the pldb database being used, stored in an atom
-  facts         -- pldb facts to remove"
-  [db name logic-db-atom & facts]
-  (let [new-db (apply pldb/db-retractions @logic-db-atom facts)]
-    (put! trans-chan [db name new-db])
-    (reset! logic-db-atom new-db)))
+(defn rm-facts!
+  "Removes facts and stores the new state."
+  [a & facts]
+  (-pldb-remove! a facts))
 
 (defn reset-facts!
-  "Resets the logic db to a new set of facts and stores the new state
+  "Resets the logic db to a new set of facts and stores the new state"
+  [a & facts]
+  (-pldb-reset! a facts))
 
-  db            -- the IndexedDB database defined with clodexeddb.core.setup
-  name          -- the text name of your in-memory database, used as a primary
-                    key for IndexedDB
-  logic-db-atom -- the pldb database being used, stored in an atom
-  facts         -- a new set of pldb facts"
-  [db name logic-db-atom & facts]
-  (let [new-db (apply pldb/db facts)]
-    (put! trans-chan [db name new-db])
-    (reset! logic-db-atom new-db)))
-
-(defn init-facts
-  "Either instantiates new datums if the given database
-  does not exist or, if there there's no database, loads an
-  existing database from clientside storage.
-
-  db            -- the IndexedDB database defined with clodexeddb.core.setup
-  name          -- the text name of your in-memory database, used as a primary
-                    key for IndexedDB
-  logic-db-atom -- the pldb database being used, stored in an atom
-  facts         -- facts from the pldb database"
-  [db name logic-db-atom & facts]
-  (cldb/get-query db "database" name
-                  (fn [e]
-                    (if (= e js/undefined)
-                      ;; FIXME: apply pldb/db here
-                      (let [new-db (apply pldb/db facts)]
-                        (put! trans-chan [db name new-db])
-                        (reset! logic-db-atom new-db))
-                      (let [new-db (-> (get e "value")
-                                     (reader/read-string))]
-                        (reset! logic-db-atom new-db))))))
-
-(defn clear
-  "Erases all state from a database (clears an object store in clientside
-  storage)."
-  [db name]
-  (cldb/clear db "database" name))
-
-(defn rm-db
-  "Removes all databases from clientside storage (deletes the IndexedDB database
-  for your project). Use the same db name from setup."
-  [db-name]
-  (cldb/rm-db db-name))
+(defn clear!
+  "Clears the atom and all associated data stores from clientside storage but
+  keeps the clientside db. With the option ':delete true', the clientside db
+  and all data will be permenantely deleted (making all connected db-atoms
+  unsuable)."
+  [a & {:keys [delete]}]
+  (-pldb-clear! a delete))
